25d466b2c7fb7253adfaec43d945dd36a7effa1c
elvisjsAtlas1 <chat02jselvis@gmai.com>
2025-04-23 01:30:51 -0500

    actualizar

--- eja-matricula-service/src/main/java/com/eja/matricula/Feign/CursoFeign.java
+++ eja-matricula-service/src/main/java/com/eja/matricula/Feign/CursoFeign.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="hlca-curso-servicio", path = "/curso")
+@FeignClient(name="eja-course-service", path = "/curso")
 public interface CursoFeign {
     @GetMapping("/{id}")
     public ResponseEntity<CursoDto> buscarCurso(@PathVariable Long id);
--- eja-matricula-service/src/main/java/com/eja/matricula/Feign/EstudianteFeign.java
+++ eja-matricula-service/src/main/java/com/eja/matricula/Feign/EstudianteFeign.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="hlca-estudiante-servicio", path = "/estudiante")
+@FeignClient(name="eja-student-service", path = "/estudiante")
 public interface EstudianteFeign {
     @GetMapping("/{id}")
     public ResponseEntity<EstudianteDto> buscarEstudiante(@PathVariable Long id);
