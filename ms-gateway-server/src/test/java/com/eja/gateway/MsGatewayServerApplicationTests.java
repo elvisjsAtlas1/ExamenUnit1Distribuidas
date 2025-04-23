31e36e7910563ed6d4d6db9aac35a0a6540b652f
elvisjsAtlas1 <chat02jselvis@gmai.com>
2025-04-22 20:21:52 -0500

    actualizando gateway

---
+++ ms-gateway-server/src/test/java/com/eja/gateway/MsGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eja.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
