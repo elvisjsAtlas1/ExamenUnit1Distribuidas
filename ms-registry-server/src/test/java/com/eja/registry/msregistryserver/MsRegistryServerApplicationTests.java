dab4c03492ac7aaf73f46efa1fb08c6bfce765e6
elvisjsAtlas1 <chat02jselvis@gmai.com>
2025-04-22 20:00:55 -0500

    actualizando

---
+++ ms-registry-server/src/test/java/com/eja/registry/msregistryserver/MsRegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eja.registry.msregistryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
